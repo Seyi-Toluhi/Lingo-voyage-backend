1879ad9259aadc9f51db039e716b7e5b25f7ef54
Oluwaseyi Toluhi <seyi@Oluwaseyis-Laptop.local>
2024-08-23 11:18:42 +0100

    tests added for landing page

--- frontend/tests/index.test.js
+++ frontend/tests/index.test.js
@@ -1,24 +1,93 @@
 const fs = require('fs');
 const path = require('path');
 const { JSDOM } = require('jsdom');
+const { fireEvent, getByText } = require('@testing-library/dom');
 
-describe('HTML page has login tool', () => {
+// testing that landing page has required buttons, links and text
+
+describe('Landing page has required buttons and text', () => {
+  test('profile link exists', () => {
+  const htmlFilePath = path.resolve(__dirname, '../index.html');
+  const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
+  const dom = new JSDOM(htmlContent, { url: 'http://localhost' });
+  const { document } = dom.window;
+  const profileLink = document.querySelector('.user_profile') 
+
+  expect(document.querySelector('.user_profile')).not.toBeNull()  
+  expect(profileLink.textContent).not.toBe("Profil") 
+  expect(profileLink.textContent).toBe("Profile") 
+}) 
+
+  test('learn button exists', () => {
+  const htmlFilePath = path.resolve(__dirname, '../index.html');
+  const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
+  const dom = new JSDOM(htmlContent, { url: 'http://localhost' });
+  const { document } = dom.window;
+  const learnButton = document.querySelector('.dropdown button')
+ 
+  expect(learnButton).not.toBeNull()  
+  expect(learnButton.textContent).toContain("Learn")  
+}) 
+
   test('login link exists', () => {
   const htmlFilePath = path.resolve(__dirname, '../index.html');
   const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
   const dom = new JSDOM(htmlContent, { url: 'http://localhost' });
   const { document } = dom.window;
 
-  expect(document.querySelector('#game_description')).not.toBeNull()  
   expect(document.querySelector('#get_login_form')).not.toBeNull() 
+  expect(document.querySelector('#get_login_form a').textContent).toBe("Log in")
+}) 
+
+  test('signup link exists', () => {
+    const htmlFilePath = path.resolve(__dirname, '../index.html');
+    const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
+    const dom = new JSDOM(htmlContent, { url: 'http://localhost' });
+    const { document } = dom.window;
+
+    expect(document.querySelector('#get_sign_up_form')).not.toBeNull() 
+    expect(document.querySelector('#get_sign_up_form a').textContent).toBe("Sign up")
+  }) 
+
+  test('app name renders on webpage', () => {
+  const htmlFilePath = path.resolve(__dirname, '../index.html');
+  const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
+  const dom = new JSDOM(htmlContent, { url: 'http://localhost' });
+  const { document } = dom.window;
+
+  const appName = document.querySelector("header")
+  expect(appName.textContent).toBe("Lingo Voyage")  
+  
 }) 
   test('game description renders on webpage', () => {
   const htmlFilePath = path.resolve(__dirname, '../index.html');
   const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
   const dom = new JSDOM(htmlContent, { url: 'http://localhost' });
   const { document } = dom.window;
-  const learn_yoruba = document.querySelector('#learn_yoruba')
-  expect(learn_yoruba.textContent).toBe(`Yoruba`)  
+
+  const textCenter = document.querySelector('.text-center')
+  expect(textCenter.textContent).not.toBeNull 
   
 }) 
   });
+
+// testing that buttons on landing page fetch required form and pages
+
+// describe('testing that buttons on landing page fetch required form and pages', () => {
+
+//   test('Profile button fetches profile page', () => {
+//   const htmlFilePath = path.resolve(__dirname, '../index.html');
+//   const htmlContent = fs.readFileSync(htmlFilePath, 'utf-8');
+//   const dom = new JSDOM(htmlContent, { url: 'http://localhost' });
+//   const { document } = dom.window;
+//   const profileLink = document.querySelector('.user_profile')
+//   fireEvent.click(profileLink)
+
+//   const generatedpage = document.querySelector("#profile_content")
+//   console.log(generatedpage)
+
+//   expect(generatedpage).not.toBeNull() 
+// }) 
+//   });
+
+//try using Playwright to simulate the webpage
